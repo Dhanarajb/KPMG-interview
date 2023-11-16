9c3cb62fb6c40752799a1be3a3e9dbcaee4ed931
Dhanarajb <88299676+Dhanarajb@users.noreply.github.com>
2023-11-17 00:20:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KPMG-interview
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Dhanarajb/KPMG-interview)
---
+++ index.js
@@ -0,0 +1,12 @@
+let array = ["Sachin", "Mohan", "Rohan", "Siddarth"]
+const removeMohan = array.filter(item=>item !== "Mohan")
+console.log(removeMohan)
+
+let arr1 = [1,2,3,4];
+let arr2 = [12,34,5,43,3]
+let mergingArray = [...arr1,...arr2]
+console.log(mergingArray)
+
+const numbers = [23, 55, 21, 87, 56];
+const maxNum = Math.max(...numbers)
+console.log(maxNum);
